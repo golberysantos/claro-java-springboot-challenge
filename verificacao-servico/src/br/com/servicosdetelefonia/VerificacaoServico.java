620eba4dc2beda65c83fb959f4743173dc9bb127
golberysantos <golbery.santos@yahoo.com.br>
2026-06-25 16:51:34 -0400

    feat: deafio Explorando Serviços de Telefonia

---
+++ verificacao-servico/src/br/com/servicosdetelefonia/VerificacaoServico.java
@@ -0,0 +1,23 @@
+package br.com.servicosdetelefonia;
+
+import java.util.Scanner;
+
+public class VerificacaoServico {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Entrada do serviço a ser verificado
+		String servico = scanner.nextLine().trim();
+
+		// Entrada do nome do cliente e os serviços contratados
+		String entradaCliente = scanner.nextLine().trim();
+
+		// Separando o nome do cliente e os serviços contratados
+		String[] partes = entradaCliente.split(",");
+		String nomeCliente = partes[0];
+		boolean contratado = false;
+
+
+		scanner.close();
+	}
+}
