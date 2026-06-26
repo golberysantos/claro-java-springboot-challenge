5683d20cf70e60726f793b0d702ece173527d9c3
golberysantos <golbery.santos@yahoo.com.br>
2026-06-25 21:54:18 -0400

    feat: deafio Explorando Serviços de Telefonia

--- verificacao-servico/src/br/com/servicosdetelefonia/VerificacaoServico.java
+++ verificacao-servico/src/br/com/servicosdetelefonia/VerificacaoServico.java
@@ -13,11 +13,24 @@ public class VerificacaoServico {
 		String entradaCliente = scanner.nextLine().trim();
 
 		// Separando o nome do cliente e os serviços contratados
-		String[] partes = entradaCliente.split(",");
-		String nomeCliente = partes[0];
+		String[] partes = entradaCliente.split(",");		
 		boolean contratado = false;
 
+		// TODO: Verifique se o serviço está na lista de serviços contratados
+		servico = servico.toLowerCase();
+		
+		
+		for (int i = 1; i < partes.length; i++) {
+			
+			if (partes[i].toLowerCase().equals(servico)) {
+		        contratado = true;
+		        break;
+			}
+		}
+
+		System.out.println(contratado ? "Sim" : "Nao");
 
 		scanner.close();
 	}
+
 }
